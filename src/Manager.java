d01c6119656e537c6983d21b6ed2b1888b6412a4
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-10 08:42:30 +0900

    Add method to display seating arrangement

--- src/Manager.java
+++ src/Manager.java
@@ -1,5 +1,17 @@
 public class Manager {
     public static void main(String[] args) {
+        // call method to print seating arrangement
+        displaySeatingArrangement();
+    }
 
+    public static void displaySeatingArrangement() {
+        System.out.print("Cinema: \n  ");
+        // display columns
+        for (int i = 1; i <= 8; i++) System.out.print(i + " ");
+        // display rows
+        for (int i = 1; i <= 7; i++) {
+            System.out.print("\n" + i + " ");
+            for (int j = 1; j <= 8; j++) System.out.print("S ");
+        }
     }
 }
