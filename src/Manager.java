50d7a4e0c78c3db4d8cb985f1bdf83a131f4a9fb
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-10 13:56:01 +0900

    Create method to fill an array with seating
    The new method returnSeatingArrangement accepts the user-inputed rows and seatsPerRow in order to create a 2d array that displays the theater's seating arrangement.

--- src/Manager.java
+++ src/Manager.java
@@ -1,18 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Manager {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // call method to print seating arrangement
-        displaySeatingArrangement();
-
-        // calculate profit
+        // get rows and seats-per-row
         System.out.print("Enter the number of rows: ");
         int rows = scanner.nextInt();
         System.out.print("Enter the number of seats in each row: ");
         int seatsPerRow = scanner.nextInt();
-        System.out.printf("Total income:%n$%d", calculateProfit(rows, seatsPerRow));
+
+        // create 2d array
+        String[][] seatingArrangement = returnSeatingArrangement(rows, seatsPerRow);
+        System.out.println(Arrays.deepToString(seatingArrangement));
+
+//        System.out.printf("Total income:%n$%d", calculateProfit(rows, seatsPerRow));
 
     }
 
@@ -26,6 +29,17 @@ public class Manager {
             for (int j = 1; j <= 8; j++) System.out.print("S ");
         }
         System.out.println();
+    }
+
+    public static String[][] returnSeatingArrangement(int rows, int seatsPerRow) {
+        String[][] seatingArrangement = new String[rows][seatsPerRow];
+        String emptySeatSymbol = "S";
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < seatsPerRow; j++) {
+                seatingArrangement[i][j] = emptySeatSymbol;
+            }
+        }
+        return seatingArrangement;
     }
 
     public static int calculateProfit(int rows, int seatsPerRow) {
